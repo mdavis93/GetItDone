77d335a5d7899dbc5750f52b5461b718974caeb6
Michael Davis <mdavis93@hotmail.com>
2018-06-21 02:15:36 -0500

    Items destroy via AJAX

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :all_items, only: [:index, :create]
+  before_action :all_items, only: [:index, :create, :destroy]
   respond_to :html, :js
 
   def new
@@ -17,7 +17,16 @@ class ItemsController < ApplicationController
       format.json{ render json: @item.errors.full_messages, status: :unprocessable_entity}
       puts @item.inspect
     end
+  end
 
+  def destroy
+    @item = Item.find(params[:id])
+
+    @item.destroy
+    respond_to do |f|
+      f.html { redirect_to root }
+      f.js
+    end
   end
 
   private
